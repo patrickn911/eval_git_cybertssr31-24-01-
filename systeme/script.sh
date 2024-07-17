d01524e7b1f79f1b26a0acfd093225c43669eead
patrickn911 <nguyenpatrick1@gmail.com>
2024-07-17 22:35:10 +0200

    Update script.sh

--- systeme/script.sh
+++ systeme/script.sh
@@ -1,11 +1,11 @@
 
 # Afficher un message pour demander l'emplacement du projet
-echo "Où voulez-vous enregistrer le projet ?"
+echo "Où voulez-vous enregistrer le projet ? C:/Users/nguye/OneDrive/Bureau/eval_git_cybertssr31-24-01-/systeme"
 # Lire l'emplacement racine
 read directory
 
 # Afficher un message pour demander le nom du projet
-echo "Quel est le nom de votre projet ?"
+echo "Quel est le nom de votre projet ? projet"
 # Lire le nom du projet
 read project
 
@@ -22,3 +22,15 @@ touch index.html style.css readme.md
 
 # Afficher un message de confirmation
 echo "Le projet a été ajouté."
+
+# Afficher le status du dossier
+git statuts
+
+# Ajouter les modifications à l'index Git
+git add .
+
+# Valider les modifications avec le message "Ajout du script.sh"
+git commit -m "Ajout du script.sh et dossier projet"
+
+# Envoyer les modifications vers le repository GitHub
+git push origin main
